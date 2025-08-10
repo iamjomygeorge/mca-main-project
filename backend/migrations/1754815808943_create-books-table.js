f10ab079de808a31ea99bb3b2975a240ad33d941
Jomy George <jomygeorge@duck.com>
2025-08-10 21:50:50 +0530

    feat(database): create books table via migration

---
+++ backend/migrations/1754815808943_create-books-table.js
@@ -0,0 +1,45 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('books', {
+    id: {
+      type: 'uuid',
+      primaryKey: true,
+      default: pgm.func('gen_random_uuid()'),
+    },
+    author_id: {
+      type: 'uuid',
+      notNull: true,
+      references: '"users"(id)',
+      onDelete: 'CASCADE',
+    },
+    title: {
+      type: 'varchar(255)',
+      notNull: true,
+    },
+    description: {
+      type: 'text',
+    },
+    cover_image_url: {
+      type: 'text',
+    },
+    book_file_url: {
+      type: 'text',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('books');
+};
