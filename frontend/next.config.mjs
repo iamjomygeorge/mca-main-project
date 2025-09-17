151092ae7ebda8e3ba1414634aae1c232a85c18a
Jomy George <jomygeorge@duck.com>
2025-09-17 19:57:52 +0530

    feat(images): configure next.js to allow remote images from S3

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mca-main-project.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
